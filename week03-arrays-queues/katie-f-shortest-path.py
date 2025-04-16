250e229ffe911eab3c5246b0b1bf9848504d2fee
Katherine Fournier <61601784+KatieFournier1@users.noreply.github.com>
2025-04-16 12:27:46 -0700

    work on shortest-route

--- week03-arrays-queues/katie-f-shortest-path.py
+++ week03-arrays-queues/katie-f-shortest-path.py
@@ -4,33 +4,55 @@
 # Shortest Path
 
 
-import copy
-
-
 class City:
     def __init__(self, name):
         self.name = name
-        self.neighbors = {}
+        self.neighbors = []
+        self.weights = []
 
-    def add_neighbor(self, other_city, travel_cost):
-        self.neighbors[other_city] = travel_cost
-        if self not in other_city.neighbors:
-            other_city.neighbors[self] = travel_cost
+    def add_connection(self, city, weight):
+        if city not in neighbors:
+            self.neighbors.append(city)
+            self.weights.append(weight)
+        else:
+            raise ValueError('neighbor already added')
 
-class Map:
+
+def init_cities(city_names: [str], weights: [int]) -> {str: City}:
+    n = len(city_names)
+    cities = {name: City(name) for name in city_names}
+    for i in range(1, n):
+        city1 = city_names[i-1]
+        weights = weights[i]
+        for j in range(i, len(weights)):
+            weight = weights[i]
+            if weight ~= 0:
+                city2 = city_names[j]
+                cities[city1].add_connection(city2, weight)
+                cities[city2].add_connection(city1, weight)
+    return cities
 
 
-class Route:
-    def __init__(self, starting_city: City):
-        self.cities = [starting_city]
-        self.cost = 0
+def traverse(starting_city: City, target_city: City, cities: [City],
+             routes: [[City]], total_costs: [int]):
+    if routes == []:
+        routes.append(starting_city)
+    for route in routes:
+        if route[-1] = starting_city:
+            for next_city in starting_city.neighbors:
+                # TODO: complete
+                pass
+
 
 def main():
-    city_names = ['Bend', 'Medford', 'K Falls', 'Reno', 'S.F']
-    cities = {City(city_name) for city_name in city_names}
-
-
-
+    city_names = ['Bend', 'Medford', 'K Falls', 'Reno', 'S.F.']
+    weights = [[0,  50,  40,  0,   0  ],
+               [50, 0,   0,   0,   200],
+               [40, 0,   0,   130, 175],
+               [0,  0,   130, 0,   180],
+               [0,  200, 175, 180, 0  ]]
+    cities = init_cities(city_names, weights)
+    routes = []
 
 
 if __name__ == '__main__':
