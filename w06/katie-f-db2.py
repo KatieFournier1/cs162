e2dce1c21e8c89c08eb90195e5b750dd6c0539bb
Katie Fournier <61601784+KatieFournier1@users.noreply.github.com>
2025-05-10 14:04:27 -0700

    extract table creation

--- w06/katie-f-db2.py
+++ w06/katie-f-db2.py
@@ -1,20 +1,13 @@
 # Katie Fournier
-# CS 162 Week 05
-# SQLite3 Database 1
+# CS 162 Week 06
+# SQLite3 Database 2
 
 
 import sqlite3
 from pathlib import Path
 
 
-def main():
-    db_file = "db1.db"
-    # Make sure we are starting with an empty database file
-    Path(db_file).unlink(missing_ok = True)
-
-    connection = sqlite3.connect(db_file)
-    cursor = connection.cursor()
-
+def create_artists_table(connection: sqlite3.Connection, cursor: sqlite3.Cursor) -> None:
     cursor.execute("""--sql
         CREATE TABLE music_artists (artist text, genre text, number_recordings integer)
         """
@@ -22,8 +15,7 @@ def main():
     cursor.executemany("""--sql
         INSERT INTO music_artists VALUES (?, ?, ?)
         """,
-        [
-            ('Miley', 'Rock', 14),
+        [   ('Miley', 'Rock', 14),
             ('Dolly', 'Country', 123),
             ('Eminem', 'HipHop', 98),
             ('Brittany', 'Rock', 37)
@@ -31,6 +23,8 @@ def main():
     )
     connection.commit()
 
+
+def create_genres_table(connection: sqlite3.Connection, cursor: sqlite3.Cursor) -> None:
     cursor.execute("""--sql
         CREATE TABLE genres (genre text, city text)
         """
@@ -38,27 +32,15 @@ def main():
     cursor.executemany("""--sql
         INSERT INTO genres VALUES (?,?)
         """,
-        [
-           ('Rock', 'Los Angeles'),
+        [  ('Rock', 'Los Angeles'),
            ('Hippie', 'Eugene'),
            ('Opera', 'Florence')
         ]
     )
     connection.commit()
-    for genre in cursor.execute("SELECT * FROM genres"):
-        print(genre)
 
-    print()
-    artists_in_genre_table = list(cursor.execute("""--sql
-        SELECT artist
-        FROM music_artists AS ma
-        JOIN genres AS g
-            ON ma.genre = g.genre
-        """
-    ))
-    for artist in artists_in_genre_table:
-        print(artist)
 
+def create_cities_table(connection: sqlite3.Connection, cursor: sqlite3.Cursor) -> None:
     cursor.execute("""--sql
         CREATE TABLE cities (city text, state text, zip_code integer, population integer)
         """
@@ -66,14 +48,44 @@ def main():
     cursor.executemany("""---sql
         INSERT INTO cities VALUES (?,?,?,?)
         """,
-        [
-            ('Los Angeles', 'CA', 66666, 10_000_000),
+        [   ('Los Angeles', 'CA', 66666, 10_000_000),
             ('Eugene', 'OR', 55555, 80_000),
             ('Nashville', 'TN', 11111, 1_500_000)
         ]
     )
     connection.commit()
- 
+
+
+
+def main():
+    # create the database file
+    db_file = "db2.db"
+    db_path = Path(db_file)
+    # Make sure we are starting with an empty database file
+    if db_path.exists():
+        print("Deleted db2.db\n")
+    Path(db_file).unlink(missing_ok = True)
+    connection = sqlite3.connect(db_file)
+    cursor = connection.cursor()
+
+    create_artists_table(connection, cursor)
+    create_genres_table(connection, cursor)
+    create_cities_table(connection, cursor)
+
+    artists_in_genre_table = list(cursor.execute("""--sql
+        SELECT artist
+        FROM music_artists AS ma
+        JOIN genres AS g
+            ON ma.genre = g.genre
+        """
+    ))
+
+    for genre in cursor.execute("SELECT * FROM genres"):
+        print(genre)
+    print()
+    for artist in artists_in_genre_table:
+        print(artist)
+
     print()
     lookup_artist__raw__ = input('Which artist would you like to know more about? ')
     if lookup_artist__raw__ == "":
@@ -101,13 +113,9 @@ def main():
 
     num_results = len(artist_query_results)
     if num_results == 0:
-        raise Exception(
-            f"database does not contain the artist '{lookup_artist__raw__}'"
-        )
+        raise Exception(f"database does not contain the artist '{lookup_artist__raw__}'")
     elif num_results > 1:
-        raise Exception(
-            f"expected 1 result for '{lookup_artist__raw__}' but got {num_results}"
-        )
+        raise Exception(f"expected 1 result for '{lookup_artist__raw__}' but got {num_results}")
     artist_info = artist_query_results[0]
 
     genre, artist, number_recordings = artist_info[0:3]
