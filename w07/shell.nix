5b7d0b2f8cd44a5fffcbe8d5970b02844dcb33b9
Katie Fournier <61601784+KatieFournier1@users.noreply.github.com>
2025-05-18 19:43:55 +0000

    update

--- w07/shell.nix
+++ w07/shell.nix
@@ -2,11 +2,11 @@ let
   nixpkgs = import <nixpkgs> {};
 in nixpkgs.mkShell {
   packages = [
-    (nixpkgs.python313Full.withPackages (pypkgs: with pypkgs; [
+    (nixpkgs.python314Full.withPackages (pypkgs: with pypkgs; [
     ]))
   ];
   shellHook = ''
-    fish --init-command="eval #(ssh-agent-c); ssh-add ~/.ssh/github"
+    fish --init-command="eval $(ssh-agent -c); ssh-add ~/.ssh/github"
     exit
   '';
 }
