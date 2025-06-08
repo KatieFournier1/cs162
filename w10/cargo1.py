828074459c853e16a3bce0c83a869c70207c0227
Katie Fournier <61601784+KatieFournier1@users.noreply.github.com>
2025-06-08 22:08:53 +0000

    implement 11b part A

--- w10/cargo1.py
+++ w10/cargo1.py
@@ -3,7 +3,9 @@
 
 
 from copy import copy
+import random
 
+print(random.randint(1,28))
 
 def load_trucks(cargo, truck1, truck2):
     if len(cargo) == 0:
@@ -12,22 +14,21 @@ def load_trucks(cargo, truck1, truck2):
         for truck in [truck1, truck2]:
             if crate + sum(truck) <= 14:
                 truck.append(crate)
-                new_truck1, new_truck2 = load_trucks(cargo[1:], copy(truck1), copy(truck2))
-                if sum(new_truck1) == 14 and sum(new_truck2) == 14:
-                    return (new_truck1, new_truck2)
-    return ([], [])
-
+                truck1, truck2 = load_trucks(cargo[1:], copy(truck1), copy(truck2))
+                if sum(truck1) == 14 and sum(truck2) == 14:
+                    return (truck1, truck2)
+    return (truck1, truck2)
 
 def main():
-    cargo = [9, 7, 3, 4, 5]
-    print (cargo, "has a sum of" , sum(cargo))
+    cargo = []
+    while (sum(cargo) < 28):
+        crate = random.randint(1, 28)
+        cargo.append(crate)
+    print (cargo, sum(cargo))
     cargo.sort(reverse=True)
     truck1, truck2 = load_trucks(cargo, [], [])
     for truck in [truck1, truck2]:
         print (truck, "has a sum of", sum(truck))
 
-
 if __name__ == "__main__":
     main()
-
-
