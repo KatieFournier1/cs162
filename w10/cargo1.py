ff14dd23743592e1619c3eb52c0b09c27081fec3
Katie Fournier <61601784+KatieFournier1@users.noreply.github.com>
2025-06-08 21:04:47 +0000

    implement part C

--- w10/cargo1.py
+++ w10/cargo1.py
@@ -2,8 +2,11 @@
 # Katie Fournier
 
 
-cargo = [9, 7, 3, 4, 5]
-print (cargo, "has a sum of " , sum(cargo))
+# cargo = [9, 7, 3, 4, 5]
+# cargo = [1, 2, 13, 12]
+# cargo = [7, 6, 5, 4, 3, 2, 1]
+cargo = [1, 2, 3, 4, 5, 6, 7]
+print (cargo, "has a sum of" , sum(cargo))
 truck1 = []
 truck2 = []
 for crate in cargo:
@@ -12,4 +15,5 @@ for crate in cargo:
         if crate + sum(truck) <= 14:
             truck.append(crate)
             break
-print (truck1, "has a sum of", sum(truck1))
+for truck in [truck1, truck2]:
+    print (truck, "has a sum of", sum(truck))
